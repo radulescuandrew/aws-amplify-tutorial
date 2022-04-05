1a669d59da6492947baa5b6da74e1117a53da5f7
Andrew Radulescu <andrew.radulescu@wearetribus.com>
2022-04-05 13:47:43 +0300

    Develop new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tribus Technologies!
+          Tribus Technologies Develop!
         </p>
         <a
           className="App-link"
